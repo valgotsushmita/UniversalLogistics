b77f4ca4aef5880f89a40e7518b1d30225deab86
Komal Kamble <kk830@goutsi.com>
2024-03-08 06:08:00 -0500

    Adding settlements module

---
+++ cypress/e2e/Settlements/Settlements-VendorList.cy.js
@@ -0,0 +1,24 @@
+
+
+beforeEach(()=>{
+    cy.LoginFunctionality();
+    cy.ClickonmenuOptions('Settlements');
+    
+  })
+  
+  
+    
+  describe("Verify Settlement functionality on Vendor list screen",()=>{
+   
+    it("Verify a contact is added, Edited and Deleted Succesfully", () => {
+      
+      
+    })
+    
+    it("Verify a contact is added, Edited and Deleted Succesfully", () => {
+      
+     
+    })
+    
+  
+    });
